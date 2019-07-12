757e49691a79fd3cfee66dfa3578892146b913f9
seu <seu@panopticon.re>
2019-07-13 01:41:39 +0200

    initial version

---
+++ README.md
@@ -0,0 +1,19 @@
+# quickjs-sys
+
+Low-level Rust bindings for the [QuickJS Javascript Engine](https://bellard.org/quickjs/).
+
+```toml
+# Cargo.toml
+[dependencies]
+quickjs-sys = "0.1"
+```
+
+The documentation of the C API can be found
+[here](https://bellard.org/quickjs/quickjs.html).
+
+# License
+
+The QuickJS library, written by Fabrice Bellard and Charlie Gordon as well as
+this bindings are licensed under
+
+ * MIT license, ([LICENSE](LICENSE) or https://opensource.org/licenses/MIT
---
+++ build.rs
@@ -0,0 +1,41 @@
+extern crate bindgen;
+
+use std::env;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn main() {
+    // compile quickjs
+    if cfg!(target_os = "linux") {
+        Command::new("make")
+                .current_dir("quickjs-2019-07-09")
+                .status()
+                .expect("failed to make!");
+    } else {
+        unimplemented!()
+    }
+
+    // Tell cargo to tell rustc to link the system bzip2
+    // shared library.
+    println!("cargo:rustc-link-search=quickjs-2019-07-09");
+    println!("cargo:rustc-link-lib=quickjs");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = bindgen::Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("wrapper.h")
+        .clang_arg("-Iquickjs-2019-07-09")
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/bindings.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("bindings.rs"))
+        .expect("Couldn't write bindings!");
+}
