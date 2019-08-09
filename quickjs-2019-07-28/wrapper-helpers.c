278ae571080ee0ada533313a12915f1dbad1cf9f
Kai Michaelis <kai.michaelis@9elements.com>
2019-08-09 17:54:58 +0200

    add more helper functions

--- quickjs-2019-07-28/wrapper-helpers.c
+++ quickjs-2019-07-28/wrapper-helpers.c
@@ -8,10 +8,64 @@ JSValue Helper_JS_NewBool(JSContext *ctx, JS_BOOL val) {
   return JS_NewBool(ctx, val);
 }
 
+JSValue Helper_JS_NewNull(void) {
+  return JS_NULL;
+}
+
+JSValue Helper_JS_NewUndefined(void) {
+  return JS_UNDEFINED;
+}
+
+JSValue Helper_JS_NewException(void) {
+  return JS_EXCEPTION;
+}
+
 void Helper_JS_FreeValue(JSContext *ctx, JSValue v) {
   return JS_FreeValue(ctx, v);
 }
 
 JSValue Helper_JS_DupValue(JSContext *ctx, JSValueConst v) {
   return JS_DupValue(ctx, v);
+}
+
+int Helper_JS_IsInteger(JSValueConst v) {
+  return JS_IsInteger(v);
+}
+
+int Helper_JS_IsString(JSValueConst v) {
+  return JS_IsString(v);
+}
+
+int Helper_JS_IsBool(JSValueConst v) {
+  return JS_IsBool(v);
+}
+
+int Helper_JS_IsSymbol(JSValueConst v)
+{
+  return JS_IsSymbol(v);
+}
+
+int Helper_JS_IsObject(JSValueConst v)
+{
+  return JS_IsObject(v);
+}
+
+int Helper_JS_IsNull(JSValueConst v)
+{
+  return JS_IsNull(v);
+}
+
+int Helper_JS_IsUndefined(JSValueConst v)
+{
+  return JS_IsUndefined(v);
+}
+
+int Helper_JS_IsException(JSValueConst v)
+{
+  return JS_IsException(v);
+}
+
+int Helper_JS_IsUninitialized(JSValueConst v)
+{
+  return JS_IsUndefined(v);
 }
--- wrapper.h
+++ wrapper.h
@@ -3,5 +3,16 @@
 
 JSValue Helper_JS_NewFloat64(JSContext *ctx, double d);
 JSValue Helper_JS_NewBool(JSContext *ctx, JS_BOOL d);
+JSValue Helper_JS_NewNull(void);
+JSValue Helper_JS_NewUndefined(void);
+JSValue Helper_JS_NewException(void);
 void Helper_JS_FreeValue(JSContext *ctx, JSValue v);
 JSValue Helper_JS_DupValue(JSContext *ctx, JSValueConst v);
+int Helper_JS_IsInteger(JSValueConst v);
+int Helper_JS_IsString(JSValueConst v);
+int Helper_JS_IsBool(JSValueConst v);
+int Helper_JS_IsUndefined(JSValueConst v);
+int Helper_JS_IsNull(JSValueConst v);
+int Helper_JS_IsObject(JSValueConst v);
+int Helper_JS_IsSymbol(JSValueConst v);
+int Helper_JS_IsException(JSValueConst v);
